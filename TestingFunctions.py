aeb4bcc7174ee99253077dfad8ba1d18b0198854
Ksieciu <antelius14@gmail.com>
2020-01-17 19:21:26 +0100

    Added unit testing
    Co-Authored-By: krzysiek15 <krzysiek15@users.noreply.github.com>
    Co-Authored-By: Piotr Wróblewski <piotrwroblewski89@users.noreply.github.com>

--- TestingFunctions.py
+++ TestingFunctions.py
@@ -1,6 +1,7 @@
-from stringFunctions import lemmatize_words
+from stringFunctions import lemmatize_words, get_clear_text, df_to_arr
 from Scraper import Scraper
 import unittest
+import pandas as pd
 
 
 class TestingFunctions(unittest.TestCase):
@@ -12,8 +13,23 @@ class TestingFunctions(unittest.TestCase):
         self.assertEqual(lemmatized, ['nikt', 'gdzie', 'możliwy', 'dbać'])
 
     def test_url(self):
-        test_class = Scraper("csvData3.csv")
+        test_class = Scraper("csvData3.csv", 1, 2)
         self.assertEqual(test_class.crawl_hot("wololo"), 1)
+
+    # def test_clearing_text(self):
+    #     test_string = """<a class="showTagSummary" href="https://www.wykop.pl/tag/polityka">polityka</a>!@#Olaboga!§Üa♂сука блять"""
+    #     _, cleared, _, _, _ = get_clear_text(test_string)
+    #     print(cleared)
+    #     self.assertEqual(cleared, 'olaboga')
+
+    def test_df_to_arr(self):
+        d = {'words_prob': [1, 0], 'words_count': [12, 13], 'hashtags_prob': [0.5, 1], 'hashtags_count': [2, 4]}
+        df = pd.DataFrame(data=d)
+        feat, labels = df_to_arr(df)
+        self.assertEqual(feat, [[1, 12, 0.5, 2], [0, 13, 1.0, 4]])
+
+
+
 
 
 if __name__ == '__main__':
--- main.py
+++ main.py
@@ -4,11 +4,15 @@ from sklearn import linear_model, naive_bayes, ensemble
 from testing_countvect import cv_prediction
 
 # data that will be predicted
-post_input = """Zastanawiasz sie dlaczego jeszcze nie przyszło Ci stypendium? Ministerstwo nie ma środków do wypłacenia na stypendia rekotorskie oraz socjalne dla studenciaków. Jak tam ten budżet bez deficytu!? XD
+post_input = """W @tvp_info ALARM, powtarzam ALARM!!
 
-Bynajmniej na bombelki dały xDDDD
+Wszystkie ręce na pokład!
 
-#studbaza #pis #bekazpisu"""
+#polityka
+
+Ale będzie się działo o 19:30
+
+#4konserwy #neuropa #tvpis"""
 
 # reading data from file
 train = pd.read_csv("csvData4.csv")
@@ -18,5 +22,5 @@ post_df = post.add_to_df()
 
 # getting new df with columns containing predictions_proba on words and hashtags
 cv_prediction(train, post_df, naive_bayes.MultinomialNB())
-# cv_prediction(train, post_df, linear_model.LogisticRegression())
-# cv_prediction(train, post_df, ensemble.RandomForestClassifier())
+cv_prediction(train, post_df, linear_model.LogisticRegression())
+cv_prediction(train, post_df, ensemble.RandomForestClassifier())
